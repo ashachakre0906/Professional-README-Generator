5991ff157d987db755c841e935b4d437e25dc748
ashachakre0906 <chourpagar.asha@gmail.com>
2022-06-20 22:03:19 -0700

    added gitignore,README.MD,creating function to initialize the app, created function to write README file,added an array of questions for user input

---
+++ index.js
@@ -0,0 +1,33 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js')
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type : 'input',
+        name : 'title',
+        message : 'What is the name of your project ?',
+    }
+    {
+
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFileSync(location,(data,"utf-8"))
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((response) => {
+        writeToFile('README.md',generateMarkdown(response))
+        console.log(response);
+    })
+}
+
+// Function call to initialize app
+init();
+
