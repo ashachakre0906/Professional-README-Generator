80e52e22e24aac372183d3c90d66986fe8707342
ashachakre0906 <chourpagar.asha@gmail.com>
2022-06-22 18:52:45 -0700

    updated the path ofd the readme.md file and added if else conditional statement

--- index.js
+++ index.js
@@ -1,89 +1,76 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
-const inquirer = require('inquirer');
-const fileName = './README.md';
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateReadMe = "./utils/generateMarkdown.js";
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type : 'input',
-        name : 'title',
-        message : 'What is the title of your project ?',
-    }
-    ,
-    {
-        type: 'input',
-        name: 'Description',
-        message: 'Please provide a short description of your project?',
-    }
-    ,
-    {
-       type: 'input',
-       name: 'Installation',
-       message:'What are the steps required to install your project?Provide a step-by-step description of how to get the development environment running?',
-    
-    }
-    ,
-    {
-        type: 'input',
-        name: 'Usage',
-        message:'Provide instructions and examples for use?',
-    }
-    ,
-    {
-        type: 'checkbox',
-        name: 'License',
-        message:'Choose the appropriate license for this project?',
-        choices:['Apache License','GNU','MIT','Rust','WordPress']
-      
-    }
-    ,
-    {
-         type: 'input',
-         name: 'Contributing',
-         message: 'Would you like other developers to contribute?',
-    }
-    ,
-    {
-        type: 'input',
-        name: 'questions',
-        message:'What do i do if i have any questions?',
-   }
-   ,
-   {
-    type: 'input',
-    name: 'gitHub username',
-    message:'Please provide your gitHub username ?',
-},
   {
-   
-    type: 'input',
-    name: 'email',
-    message:'Please provide your email address?',
-   
-  }
-  ,
+    type: "input",
+    name: "title",
+    message: "What is the title of your project ?",
+  },
+  {
+    type: "input",
+    name: "Description",
+    message: "Please provide a short description of your project?",
+  },
+  {
+    type: "input",
+    name: "Installation",
+    message:
+      "What are the steps required to install your project?",
+  },
+  {
+    type: "input",
+    name: "Usage",
+    message: "Provide instructions and examples for use?",
+  },
+  {
+    type: "checkbox",
+    name: "License",
+    message: "Choose the appropriate license for this project?",
+    choices: ["Apache", "GNU", "MIT", "Mozilla", "IBM"],
+  },
+  {
+    type: "input",
+    name: "Contributing",
+    message: "Would you like other developers to contribute?",
+  },
+  {
+    type: "input",
+    name: "questions",
+    message: "What do i do if i have any questions?",
+  },
+  {
+    type: "input",
+    name: "gitHub username",
+    message: "Please provide your gitHub username ?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please provide your email address?",
+  },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    try {
-        fs.writeFile('fileName',data,'utf-8','err');
-        console.log('Your README.md file is sucessfully generated')
-    } catch (err){
-        console.error(err);
-    }
+  try {
+    fs.writeFile("generateReadMe", data, "utf-8", "err");
+    console.log("Your README.md file is sucessfully generated");
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    console.log('Welcome to node.js,Get ready to answer the following prompts')
-    inquirer.prompt(questions)
-    .then((response) => {
-        writeToFile('README.md',(response))
-        console.log(response);
-    })
+  console.log("Welcome to node.js,Get ready to answer the following prompts");
+  inquirer.prompt(questions).then((response) => {
+    writeToFile("./README.md", response);
+    console.log(response);
+  });
 }
 
 // Function call to initialize app
 init();
-
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,26 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+let licenseType = license;
+let yourLicense = '';
+let noLicense;
 function renderLicenseBadge(license) {
+  if(licenseType === 'Apache'){
+    return yourLicense ==='[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  }
+  else if (licenseType === 'GNU'){
+    return yourLicense === '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  
+  }
+  else if (licenseType === 'MIT'){
+    return yourLicense === '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  }
+  else if (licenseType === 'Mozilla'){
+    return yourLicense === '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+  }
+  else if (licenseType === 'IBM'){
+    return yourLicense === '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
+  }
+  else return noLicense;
 
 }
 
