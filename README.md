18d9f2e6ffd901ba462ec41b647c22b2729ddaf2
ashachakre0906 <chourpagar.asha@gmail.com>
2022-06-25 15:39:49 -0700

    resolved errors in my code and refactorede

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
-# Readme Generator
-  # We need to create a command-line application that dynamically generates a professional README.md file from a user's input using the Inquirer package.The application will be invoked by using the following command node index.js
-  # repo includes a package.json with the required dependencies. You can create one by running npm init when you first set up the project, before installing any dependencies.
-  # code snippets
-  # yes please 
-  # test.js
-  # ashachakre0906
-  # chourpagar.asha@gmail.com
+## title 
+  dsdsf
   ## License
-  [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]
-  [![License](https://opensource.org/licenses/Apache-2.0)
+   ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+  ## Description
+  shfsgh
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [License](#license)
+  - [Tests](#tests)
+  - [Contact](#contact)
+  ## Installation
+  jfhfj
+  ## Usage
+   jfbbefw
+  ## Contributing
+  sjhfjefg
+  ## License
+    License use for the project is[![License: MIT](https://opensource.org/licenses/MIT)
+  ## tests
+   jsdhvgfdsjh
+  ## Contact
+   My Git hub username isshdvgdshjv
+   My Git hub email isjsvhsfdj
  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,20 +2,20 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if(license === 'Apache 2.0'){
-    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]'
+    return '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)'
   }
   else if (license === 'Boost'){
-    return '![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]'
+    return '![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)'
   
   }
   else if (license === 'MIT'){
-    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]'
+    return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
   }
   else if (license === 'Mozilla'){
-    return yourLicense === '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]'
+    return yourLicense === '![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)'
   }
   else if (license === 'IBM'){
-    return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)]'
+    return '![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)'
   }
   else if  (license === 'none') {
     return '';
@@ -24,24 +24,11 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if(license === 'Apache 2.0'){
-    return '[![License](https://opensource.org/licenses/Apache-2.0)'
-  }
-  else if (license === 'Boost'){
-    return '(https://www.boost.org/LICENSE_1_0.txt)'
-  }
-  else if (license === 'MIT'){
-    return'[![License: MIT](https://opensource.org/licenses/MIT)'
-  }
-  else if (license === 'Mozilla'){
-    return '[![License: MPL 2.0](https://opensource.org/licenses/MPL-2.0)'
-  }
-  else if (license === 'IBM'){
-    return '[![License: IPL 1.0](https://opensource.org/licenses/IPL-1.0)'
-  }
-  else if (license === 'none') {
+  
+  if (license === 'none') {
     return '';
   }
+  else return `[License](#license)`  
 
 }
 
@@ -51,24 +38,57 @@ function renderLicenseSection(license) {
   if (license === 'none'){
     return '';
   }
-  else {
-    return `## License`;
+  if(license === 'Apache 2.0'){
+    return  `## License
+    License use for the project is[![License](https://opensource.org/licenses/Apache-2.0)`
+  }
+  else if (license === 'Boost'){
+    return  `## License
+    License use for the project is(https://www.boost.org/LICENSE_1_0.txt)`
+  }
+  else if (license === 'MIT'){
+    return  `## License
+    License use for the project is[![License: MIT](https://opensource.org/licenses/MIT)`
+  }
+  else if (license === 'Mozilla'){
+    return  `## License
+    License use for the project is[![License: MPL 2.0](https://opensource.org/licenses/MPL-2.0)`
+  }
+  else if (license === 'IBM'){
+    return  `## License
+    License use for the project is '[![License: IPL 1.0](https://opensource.org/licenses/IPL-1.0)`
+  }
+  else if (license === 'none') {
+    return '';
   }
 }
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  # ${data.description}
-  # ${data.installation}
-  # ${data.usage}
-  # ${data.contributing}
-  # ${data.tests}
-  # ${data.username}
-  # ${data.email}
+  return `## title 
+  ${data.title}
+  ## License
+   ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - ${renderLicenseLink(data.license)}
+  - [Tests](#tests)
+  - [Contact](#contact)
+  ## Installation
+  ${data.installation}
+  ## Usage
+   ${data.usage}
+  ## Contributing
+  ${data.contributing}
   ${renderLicenseSection(data.license)}
-  ${renderLicenseBadge(data.license)}
-  ${renderLicenseLink(data.license)}
+  ## tests
+   ${data.tests}
+  ## Contact
+   My Git hub username is${data.username}
+   My Git hub email is${data.email}
  `;
 }
 
