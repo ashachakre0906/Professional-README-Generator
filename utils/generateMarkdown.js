5f92660ae5023cb0d002d094bcb3e48575a5edfc
ashachakre0906 <chourpagar.asha@gmail.com>
2022-06-26 12:15:20 -0700

    deleted the word license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,10 +2,10 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if(license === 'Apache 2.0'){
-    return '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)'
+    return '![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)'
   }
   else if (license === 'Boost'){
-    return '![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)'
+    return '![License: Boost](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)'
   
   }
   else if (license === 'MIT'){
@@ -24,7 +24,6 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  
   if (license === 'none') {
     return '';
   }
@@ -40,7 +39,7 @@ function renderLicenseSection(license) {
   }
   if(license === 'Apache 2.0'){
     return  `## License
-License use for the project is [License:Apache 2.0](https://opensource.org/licenses/Apache-2.0)`
+License use for the project is [Apache 2.0](https://opensource.org/licenses/Apache-2.0)`
   }
   else if (license === 'Boost'){
     return  `## License
@@ -64,7 +63,7 @@ License use for the project is '[License: IPL 1.0](https://opensource.org/licens
 }
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-return `## title 
+return `## Title 
 ${data.title}
 ## License
 ${renderLicenseBadge(data.license)}
@@ -84,7 +83,7 @@ ${data.usage}
 ## Contributing
 ${data.contributing}
 ${renderLicenseSection(data.license)}
-## tests
+## Tests
 ${data.tests}
 ## Contact
 [Github](https://github.com/${data.username})
