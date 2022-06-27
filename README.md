2e7663736345355a5ce6657e8d6b67cd1ec11d92
ashachakre0906 <chourpagar.asha@gmail.com>
2022-06-26 23:39:22 -0700

    added description, installation steps , code examples and screenshots

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 ## License
 ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
 ## Description
-We need to create a command-line application that dynamically generates a professional README.md file from a user's input using the Inquirer package.The application will be invoked by using the following command node index.js.
+The purpose of this project to create a command-line application that dynamically generates a professional README.md file from a user's input using the Inquirer package.`fs` for writing to the file system and`inquirer`package for collecting user input.
+The application will be invoked by using the following command `node index.js`.
 ## Table of Contents
 - [Installation](#installation)
 - [Usage](#usage)
@@ -11,15 +12,44 @@ We need to create a command-line application that dynamically generates a profes
 - [Tests](#tests)
 - [Questions](#questions)
 ## Installation
-npm install npm inquirer
+* Install Node in your computer by going to `https://nodejs.org/en/download/`
+* Create a new github repository and clone it to your local machine.
+* Navigate to the repo which you just created by typing`cd` command  and open it in your code editor by typing the command in your terminal `code .`
+* Open the terminal in your code editor,initialized the repository with a `package.json` file by running `npm init -y`.
+* Install the `Inquirer package`by typing the command `npm install inquirer` which will collect the user input.
+* Application will be invoked by using the following command:
+```bash
+node index.js
+```
 ## Usage
-Examples
+***Code Examples and Screenshots***
+![Function to render the License badge]<br>(assets/images/screenshot.png)
+***Function to write the user input to the README.md file***
+```
+function writeToFile(fileName, data) {
+  // console.log(fileName);
+  // console.log(data);
+    fs.writeFile(fileName, generateReadMe(data),'utf-8',(err) =>{
+        if (err)
+        console.log(err);
+        else {
+            console.log('"Success! Your README.md file has been generated"')
+        }
+    })
+};
+```
+
 ## Contributing
 yes
 ## License
 This project is license under the [License: MIT](https://opensource.org/licenses/MIT)
 ## Tests
 test.js
+## Technologies Used
+![Java Badge](https://img.shields.io/badge/language-Javascript-blue.svg)
+![Node.js Badge](https://img.shields.io/badge/language-Node-yellow.svg)
+![Inquirer Badge](https://img.shields.io/badge/language-Inquirer-orange.svg)
+
 ## Questions
 Github Repo URL: [Github](https://github.com/ashachakre0906)<br>
 Email Address: chourpagar.asha@gmail.com
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,68 +1,58 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if(license === 'Apache 2.0'){
-    return '![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)'
-  }
-  else if (license === 'Boost'){
-    return '![License: Boost](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)'
-  
-  }
-  else if (license === 'MIT'){
-    return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
-  }
-  else if (license === 'Mozilla'){
-    return yourLicense === '![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)'
-  }
-  else if (license === 'IBM'){
-    return '![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)'
-  }
-  else if  (license === 'none') {
-    return '';
+  if (license === "Apache 2.0") {
+    return "![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)";
+  } else if (license === "Boost") {
+    return "![License: Boost](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)";
+  } else if (license === "MIT") {
+    return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+  } else if (license === "Mozilla") {
+    return (
+      yourLicense ===
+      "![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)"
+    );
+  } else if (license === "IBM") {
+    return "![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)";
+  } else if (license === "none") {
+    return "";
   }
 }
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license === 'none') {
-    return '';
-  }
-  else return `[License](#license)`  
-
+  if (license === "none") {
+    return "";
+  } else return `[License](#license)`;
 }
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license === 'none'){
-    return '';
+  if (license === "none") {
+    return "";
   }
-  if(license === 'Apache 2.0'){
-    return  `## License
-This project is license under the [License: Apache 2.0](https://opensource.org/licenses/Apache-2.0)`
-  }
-  else if (license === 'Boost'){
-    return  `## License
-  This project is license under the [License: Boost](https://www.boost.org/LICENSE_1_0.txt)`
-  }
-  else if (license === 'MIT'){
-    return  `## License
-  This project is license under the [License: MIT](https://opensource.org/licenses/MIT)`
-  }
-  else if (license === 'Mozilla'){
-    return  `## License
-  This project is license under the[License: MPL 2.0](https://opensource.org/licenses/MPL-2.0)`
-  }
-  else if (license === 'IBM'){
-    return  `## License
-  This project is license under thes '[License: IPL 1.0](https://opensource.org/licenses/IPL-1.0)`
-  }
-  else if (license === 'none') {
-    return '';
+  if (license === "Apache 2.0") {
+    return `## License
+  This project is license under the [License: Apache 2.0](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license === "Boost") {
+    return `## License
+  This project is license under the [License: Boost](https://www.boost.org/LICENSE_1_0.txt)`;
+  } else if (license === "MIT") {
+    return `## License
+  This project is license under the [License: MIT](https://opensource.org/licenses/MIT)`;
+  } else if (license === "Mozilla") {
+    return `## License
+  This project is license under the[License: MPL 2.0](https://opensource.org/licenses/MPL-2.0)`;
+  } else if (license === "IBM") {
+    return `## License
+  This project is license under thes '[License: IPL 1.0](https://opensource.org/licenses/IPL-1.0)`;
+  } else if (license === "none") {
+    return "";
   }
 }
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-return `
+  return `
 # ${data.title}
 ## License
 ${renderLicenseBadge(data.license)}
@@ -89,4 +79,4 @@ Github Repo URL: [Github](https://github.com/${data.username})<br>
 Email: ${data.email}
  `;
 }
-module.exports = generateMarkdown;//exporting the variables, functions and objects into index.js
+module.exports = generateMarkdown; //exporting the variables, functions and objects into index.js
